ffb43cfe7b8246801975277e54d8a8cb452eba73
Akhil-m31 <akhil310ak@gmail.com>
2026-01-20 19:10:45 +0530

    Add songs array with sample music data

---
+++ js/songs.js
@@ -0,0 +1,23 @@
+const songs=[
+{
+id:1,
+title:"SoundHelix One",
+artist:"Helix",
+src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+cover:"https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4"
+},
+{
+id:2,
+title:"SoundHelix Two",
+artist:"Helix",
+src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+cover:"https://images.unsplash.com/photo-1511379938547-c1f69419868d"
+},
+{
+id:3,
+title:"SoundHelix Three",
+artist:"Helix",
+src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+cover:"https://images.unsplash.com/photo-1507874457470-272b3c8d8ee2"
+}
+]
